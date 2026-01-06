56ff720964d3600d794042ec4612e1299d6c19da
RemoveWingman <rw8+git@posteo.lu>
2026-01-06 20:13:32 +0100

    make bootstrap posix compliant and remove 401 and 402

--- configure.ml
+++ configure.ml
@@ -1,8 +1,1 @@
-let version = Sys.ocaml_version in
-ignore (Sys.command "rm -f lib/base/compat.ml");
-if version < "4.02.0" then
-  Sys.command "cp -f compat401.ml lib/base/compat.ml"
-else if version < "4.03.0" then
-  Sys.command "cp -f compat402.ml lib/base/compat.ml"
-else
-  Sys.command "cp -f compat403.ml lib/base/compat.ml"
+let version = Sys.ocaml_version
